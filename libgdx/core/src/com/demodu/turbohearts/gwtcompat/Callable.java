2e14dfd5160d12469f87592b30892a0e33964c2f
Yujin Wu <yujinwunz@example.com>
2017-04-18 02:16:34 +1000

    removed unused throws in javadoc

--- libgdx/core/src/com/demodu/turbohearts/gwtcompat/Callable.java
+++ libgdx/core/src/com/demodu/turbohearts/gwtcompat/Callable.java
@@ -6,7 +6,6 @@ public interface Callable<V> {
 	 * Computes a result, or throws an exception if unable to do so.
 	 *
 	 * @return computed result
-	 * @throws Exception if unable to compute a result
 	 */
 	V call();
 }
